0408a05bc82dcd7cf09e38b5021a3a8b8b2c80ce
Gove Allen <gove.allen@gmail.com>
2022-11-29 16:18:31 -0700

    working system

--- index.js
+++ index.js
@@ -1,129 +1,115 @@
+// works with the spreadsheet named group evals owned by unformationsystemscore
+const gas_deployment_id='AKfycbxbM_KIqQSaAC7ye3sxzcNn7O8aOl-1T0eW-C9EGGiX02zyf-TzV1XTncE02aFgLXK7vA'
+const gas_end_point = 'https://script.google.com/macros/s/'+gas_deployment_id+'/exec'
+
+
 evaluation='<select name="a"><option value=1>1-Much less<br>than average</option><option value=2>2</option><option value=3>3-About average</option><option value=4>4</option><option value=5>5-Much more than average</option>'
 let team
-const questionnaire = []
+let survey
+function fill_random(){
+    let x=0
+    for(const s of survey.questionnaire){
+        for(const q of s.questions  ){
+            
+            const rows = document.getElementsByTagName("table")[x++].rows;
 
-function configure(){
-    const params = atob(window.location.search.substr(1)).split("|")
-    document.getElementById("title").innerHTML=params[3]
-    document.getElementById("form_title").value=params[3]
-    document.getElementById("netid").value=params[0]
-    team=params[1].split(",")
-    document.getElementById("student_name").innerHTML = team[0]
-    
-
-    // remove null from the team.  Null is used because normal team size is 4, but when a team member drops the core, it is easier to substitute with null rather than reduce teh call to only 3 team members 
-    for(let i=team.length-1;i>-1;i--){
-        if(team[i].replace(/ /g, '')==="null"){
-            team.splice(i, 1)
-        }
-    }
-
-   
-    console.log("params",params)
-    const promises=[]
-    //get question sets
-    for(const qset of params[2].split(",")){
-        promises.push(
-            fetch("https://byuis.github.io/forms/group_evals/question_sets/" + qset + ".json?" + new Date().getTime())
-            .then(response => response.json())
-            .catch(error => {
-                console.log("error",error)
-            })
-        )
-    
-    }
-
-    const label_promises = []
-    const labels=[]
-    const question_labels={}
-    Promise.all(promises).then((question_sets) => {
-        //now we have the question sets, get the labels
-        for(const question_set of question_sets){
-            questionnaire.push(question_set)
-            console.log(question_set.header)
-            for(const question of question_set.questions){
-                console.log(question)   
-                if(!labels.includes(question.labels)){
-                    labels.push(question.labels)
-                    question_labels[question.labels]=null
-                    label_promises.push(
-                        fetch("https://byuis.github.io/forms/group_evals/labels/" + question.labels + ".json?" + new Date().getTime())
-                        .then(response => response.json())
-                        .catch(error => {
-                            console.log("error",error)
-                        })
-                    )
-                }     
+            for(let r=1;r< rows.length;r++){
+                const row = rows[r]
+                const random_number=randBetween(1,5)
+                //console.log(row.children.length,random_number, row.children[random_number])
+                row.children[random_number].children[0].checked=true
             }
         }
-        // now we have requested all the labels
-        Promise.all(label_promises).then((labels_data) => {
-            for(let i=0; i<labels_data.length;i++){
-                question_labels[labels[i]]=labels_data[i]
-            }
-            // now we have the labels, ready to render            
-
-            //interate labels into questionnaire
-            for(const qs of questionnaire){
-                for(const q of qs.questions){
-                    q.labels = question_labels[q.labels]
-                }
-            }
-            console.log("===========================================")
-            console.log("Questionnaire", questionnaire)
     
-           
-            for(let q=0;q<questionnaire.length;q++){
-                const question_set = questionnaire[q]
-                const newdiv=document.createElement("div")
-                newdiv.className="header"
-                newdiv.innerHTML="<h2>" + question_set.header + "</h2>"
-                document.getElementById("evals").appendChild(newdiv);
+    }
+    document.getElementById("comment").value="This survey was randomly generated"
+}
 
-                for(let e=0; e < question_set.questions.length; e++){
-                    const question = question_set.questions[e]
-                    const table=document.createElement('table')
-                    const row=table.insertRow() 
-                    const row_html=['<th class="first-col"></th>']
-                    for(const label of question.labels){
-                        if(label){
-                            row_html.push("<th>" + label + "</th>")
-                        }else{
-                            row_html.push("<th></th>")
-                        }
-                    }
-                    row.innerHTML=row_html.join("")
-                    for(let i=team.length-1;i>-1;i--){
-                 
-                        const row  = table.insertRow()
-                        row.className="normal"
-                        row.id=`row${i}q${q}e${e}`
-                        let suffix=""
-                        if(i===0){suffix=" (you)"}
-            
-                        row.innerHTML=` <td class="first-col">${team[i]}${suffix}</td>
-                        <td><input type="radio" value="1" name="tm${i}q${q}e${e}" /></td>
-                        <td><input type="radio" value="2" name="tm${i}q${q}e${e}" /></td>
-                        <td><input type="radio" value="3" name="tm${i}q${q}e${e}" /></td>
-                        <td><input type="radio" value="4" name="tm${i}q${q}e${e}" /></td>
-                        <td><input type="radio" value="5" name="tm${i}q${q}e${e}" /></td>`
-                    }
-            
-                    const evals=document.getElementById("evals")
-                    const newdiv=document.createElement("div")
-                    newdiv.innerHTML=`
-                        <h3><span class="emphasis">${question.text}</span>.</h3>
-                    `
-                    evals.appendChild(newdiv);
-                    evals.appendChild(table);
-                }
+async function configure(netid){
+    document.getElementById("netid").style.display="none"
+    document.getElementById("waiting").style.display="block"
+    survey=await server_request({netid:netid})
+    document.getElementById("waiting").style.display="none"
+    document.getElementById("body").style.display="block"
+    //const params = atob(window.location.search.substr(1)).split("|")
+    document.getElementById("title").innerHTML=survey.title
+    document.getElementById("form_title").value=survey.title
+    document.getElementById("student_netid").value=netid
+    
+    team=[survey.group[netid]]
+    for(const[key,val] of Object.entries(survey.group)){
+        if(key!==netid){
+        team.push(val)
+        }
+    }
+    
+    document.getElementById("student_name").innerHTML = survey.group[netid]
     
 
+
+    
+
+    //interate labels into survey.questionnaire
+    for(const qs of survey.questionnaire){
+        for(const q of qs.questions){
+            q.labels = survey.labels[q.labels]
+        }
+    }
+    console.log("===========================================")
+    console.log("survey.questionnaire", survey.questionnaire)
+
+    
+    for(let q=0;q<survey.questionnaire.length;q++){
+        const question_set = survey.questionnaire[q]
+        const newdiv=document.createElement("div")
+        newdiv.className="header"
+        newdiv.innerHTML="<h2>" + question_set.header + "</h2>"
+        document.getElementById("evals").appendChild(newdiv);
+
+        for(let e=0; e < question_set.questions.length; e++){
+            const question = question_set.questions[e]
+            const table=document.createElement('table')
+            const row=table.insertRow() 
+            const row_html=['<th class="first-col"></th>']
+            for(const label of question.labels){
+                if(label){
+                    row_html.push("<th>" + label + "</th>")
+                }else{
+                    row_html.push("<th></th>")
+                }
             }
+            row.innerHTML=row_html.join("")
+            for(let i=team.length-1;i>-1;i--){
+            
+                const row  = table.insertRow()
+                row.className="normal"
+                row.id=`row${i}q${q}e${e}`
+                let suffix=""
+                if(i===0){suffix=" (you)"}
+    
+                row.innerHTML=` <td class="first-col">${team[i]}${suffix}</td>
+                <td><input type="radio" value="1" name="tm${i}q${q}e${e}" /></td>
+                <td><input type="radio" value="2" name="tm${i}q${q}e${e}" /></td>
+                <td><input type="radio" value="3" name="tm${i}q${q}e${e}" /></td>
+                <td><input type="radio" value="4" name="tm${i}q${q}e${e}" /></td>
+                <td><input type="radio" value="5" name="tm${i}q${q}e${e}" /></td>`
+            }
+    
+            const evals=document.getElementById("evals")
+            const newdiv=document.createElement("div")
+            newdiv.innerHTML=`
+                <h3><span class="emphasis">${question.text}</span>.</h3>
+            `
+            evals.appendChild(newdiv);
+            evals.appendChild(table);
+        }
+
+
+    }
 
             // end of rendering survey   
-        });     
-    });
+          
+
 
 
 }
@@ -133,8 +119,8 @@ function submit_form(){
     let message=null
     let data_comment
 
-    for(let i=0;i<questionnaire.length;i++){
-        const qs = questionnaire[i]
+    for(let i=0;i<survey.questionnaire.length;i++){
+        const qs = survey.questionnaire[i]
         data[qs.header] = {}
         for(let j=0;j<qs.questions.length;j++){
             const q = qs.questions[j]
@@ -175,16 +161,8 @@ function submit_form(){
     }
     if(document.getElementById("comment").value){
       data_comment=document.getElementById("comment").value
-    }else if(window.event.ctrlKey){
-        //for debugging
-        const comment = []
-        for(let i=team.length-1;i>0;i--){
-            comment.push(team[i].split(" ")[0])
-            comment.push(", ")
-        }
-        comment[comment.length-3]=", and "
-        comment[comment.length-1]=" were really great to work with.  I can't imagine a better group.  Please keep us together next semester!!!"
-        data_comment=comment.join("")
+    }else{  
+        data_comment=""
     }
    //console.log ("data",data)
    document.getElementById("stud_name").value = team[0]
@@ -199,3 +177,35 @@ function randBetween(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
+
+async function server_request(payload, callback){
+    //This function is used to invoke a function in Google App Script.
+    //if a callback is not provided  this function waits until the call is complete
+    
+    console.log("const payload=`" + JSON.stringify(payload) + "`")//This is primarily useful for troubleshooting. The data passed to Google App Script is sent to the console.
+    //The request for Google App Script is formatted.
+    const options = { 
+        method: "POST", 
+        body: JSON.stringify(payload),
+    }
+
+    if(callback){// execute the requst and let callback handle the response
+        fetch(gas_end_point, options)
+        .then(response => response.json())
+        .then(callback);
+    }else{ //execute the request and wait for the response so it can be returned
+        const reply = await fetch(gas_end_point, options)
+        //The request is made of Google App Script and the response is set to "response"
+        const response = await reply.json()
+
+        if(response.error){
+            // we trapped an error in the google apps script
+            console.error("Error in Google Apps Script")
+            throw response.error
+
+        }
+        console.log("in post data", response)     
+
+        return response
+    }
+}
